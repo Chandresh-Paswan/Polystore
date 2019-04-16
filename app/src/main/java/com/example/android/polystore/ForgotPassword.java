0018d15156ea481b5d11eb8a04ed173cd6b203ca
Chandresh-Paswan <chandreshpas@gmail.com>
2019-04-16 10:18:20 +0530

    Removed Gson file of Firebase.

--- app/src/main/java/com/example/android/polystore/ForgotPassword.java
+++ app/src/main/java/com/example/android/polystore/ForgotPassword.java
@@ -2,7 +2,6 @@ package com.example.android.polystore;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,15 +10,10 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-
 public class ForgotPassword extends AppCompatActivity {
 
     private EditText inputEmail;
     private Button btnReset, btnBack;
-    private FirebaseAuth auth;
     private ProgressBar progressBar;
 
     @Override
@@ -31,8 +25,6 @@ public class ForgotPassword extends AppCompatActivity {
         btnReset = (Button) findViewById(R.id.btn_reset_password);
         btnBack = (Button) findViewById(R.id.btn_back);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        auth = FirebaseAuth.getInstance();
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,22 +44,10 @@ public class ForgotPassword extends AppCompatActivity {
                     return;
                 }
 
+                startActivity(new Intent(ForgotPassword.this, WebViewActivity.class));
+
                 progressBar.setVisibility(View.VISIBLE);
-                auth.sendPasswordResetEmail(email)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
 
-                                    startActivity(new Intent(ForgotPassword.this, WebViewActivity.class));
-                                    Toast.makeText(ForgotPassword.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(ForgotPassword.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
-                                }
-
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        });
             }
         });
     }
